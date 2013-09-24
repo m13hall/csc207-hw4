cd49185b27b0bd6017515e81496a33fb57a27627
m13hall <hallmira@grinnell.edu>
2013-09-23 20:11:12 -0500

    added equals

---
+++ csc207-hw4/src/FractionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FractionTest {
+
+	@Test
+	public void test_FractionFromBigInts() {
+		fail("Not yet implemented");
+	}
+
+}
